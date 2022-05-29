09453d557cd576bfb0034d0c8377289aca0749a9
VladPolonsky <vladpolonsky91@gmail.com>
2022-05-29 15:42:59 +0300

    saving project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { AddproductComponent } from './comps/addproduct/addproduct.component';
+import { CheckoutComponent } from './comps/checkout/checkout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './comps/cart/cart.component';
+import { LoginComponent } from './comps/login/login.component';
+import { MainComponent } from './comps/main/main.component';
+import { RegisterComponent } from './comps/register/register.component';
+import { AdminguardGuard } from './guards/adminguard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'users/login',pathMatch:"full"},
+  {path:'users/login',component:LoginComponent},
+  {path:'users/register',component:RegisterComponent},
+  {path:'products',component:MainComponent},
+  {path:'cart', component:CartComponent},
+  {path:'Checkout',component:CheckoutComponent},
+  {path:'addproduct',component:AddproductComponent,canActivate:[AdminguardGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { FilterPipe } from './shared/filter.pipe';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './comps/top/top.component';
+import { MainComponent } from './comps/main/main.component';
+import { LoginComponent } from './comps/login/login.component';
+import { RegisterComponent } from './comps/register/register.component';
+import { CartComponent } from './comps/cart/cart.component';
+import { CheckoutComponent } from './comps/checkout/checkout.component';
+import { AddproductComponent } from './comps/addproduct/addproduct.component';
+import { AdmincheckService } from './services/admincheck.service';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    MainComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    FilterPipe,
+    CheckoutComponent,
+    AddproductComponent,
+
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
